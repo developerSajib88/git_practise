c05a5fde09d30c5536fd8de8dccc98e418be0b98
developerSajib88 <developersajib88@gmail.com>
2023-11-22 10:34:30 +0600

    new_issues this fix #4

--- lib/second.dart
+++ lib/second.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class Second extends StatefulWidget {
+class Second extends StatelessWidget {
   const Second({super.key});
 
-  @override
-  State<Second> createState() => _SecondState();
-}
-
-class _SecondState extends State<Second> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
